c670ce4fb83c7d67edfea6a5570fe0204e49d344
mika1407 <koneautomaatio@hotmail.com>
2026-05-22 23:01:09 +0300

    css added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,5 +5,8 @@ type ButtonProps = {
 }
 
 export function Button({ children }: ButtonProps) {
-    return <button>{children}</button>
+    return <button 
+        className="bg-violet-600 hover:bg-violet-500 transition-colors 
+        rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed cursor-pointer">
+        {children}</button>
 }
